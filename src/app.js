0fa51dbc4b5cfbba8a3545640e1f74e7cf5c73ce
Oriol383 <oriolcm02@gmail.com>
2025-04-04 14:27:16 +0200

    Update app.js

--- src/app.js
+++ src/app.js
@@ -7,5 +7,16 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+let who = ['The crocodile ', 'My girlfriend ', 'The firefighter ', 'My snake '];
+let action = ['ate ', 'peed ', 'smashed ', 'cut '];
+let what = ['my homework ', 'my laptop ', 'the car '];
+let when = ['before the class ', 'when I was smoking ', 'while I was exercising ', 'during my dinner ', 'while I was singing '];
+
+let quien = Math.floor(Math.random() * who.length);
+let accion = Math.floor(Math.random() * action.length);
+let que = Math.floor(Math.random() * what.length);
+let cuando = Math.floor(Math.random() * when.length);
+
+document.querySelector("#excuse").innerHTML = who[quien] + action[accion] + what[que] + when[cuando];
 };
+
